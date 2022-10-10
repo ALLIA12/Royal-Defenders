08aae93311f1ed3d21f8afda47a19c39ea0a0c82
Ali Habibullah <ALLIA1999@HOTMAIL.COM>
2022-10-10 17:14:36 +0300

    Added a lot of stuff, too lazy to list it here.

---
+++ Assets/UI/SettingsMenu.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+using System;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public TextMeshProUGUI label;
+    public Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropDown;
+
+    private void Start()
+    {
+        AddOptions();
+
+        ChangeQuailtyUI();
+    }
+
+
+    private void AddOptions()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+        // temp value for now
+        int temp = -1;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            Resolution item = resolutions[i];
+            string option = item.width + " x " + item.height;
+            options.Add(option);
+            if (item.width == Screen.currentResolution.width &&
+                item.height == Screen.currentResolution.height)
+            {
+                temp = i; 
+            }
+        }
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = temp;
+        resolutionDropDown.RefreshShownValue();
+    }
+
+    private void ChangeQuailtyUI()
+    {
+        label = GameObject.FindGameObjectWithTag("graphicsText").GetComponent<TextMeshProUGUI>();
+        if (label == null) return;
+        string temp = QualitySettings.names[QualitySettings.GetQualityLevel()];
+        label.text = temp.ToUpper();
+    }
+
+    public void SetVolume(float decimalVolume) {
+        var dbVolume = Mathf.Log10(decimalVolume) * 20;
+        if (decimalVolume == 0.0f)
+        {
+            dbVolume = -80.0f;
+        }
+        audioMixer.SetFloat("Volume", dbVolume);
+    }
+
+    public void SetGraphics(int quailtyIndex) {
+        // This might need work later on.
+        QualitySettings.SetQualityLevel(quailtyIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen) {
+        Screen.fullScreen = isFullScreen; 
+    }
+
+    public void SetResolution(int resolutionIndex) {
+        Resolution temp= resolutions[resolutionIndex];
+        Screen.SetResolution(temp.width,temp.height, Screen.fullScreen);
+    }
+}

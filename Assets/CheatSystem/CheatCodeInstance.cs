d97f14b33ec7cafceee437c36bcf766c2913b5f4
Ali Habibullah <64721459+ALLIA12@users.noreply.github.com>
2023-01-01 16:20:55 +0300

    Level 1 re design

---
+++ Assets/CheatSystem/CheatCodeInstance.cs
@@ -0,0 +1,8 @@
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CheatCodeInstance
+{
+    public string code;
+    public UnityEvent cheatEvent;
+}
---
+++ Assets/CheatSystem/CheatCodeManager.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatCodeManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool playerTyping = false;
+    [SerializeField]
+    private string currentString = "";
+    [SerializeField]
+    private List<CheatCodeInstance> cheatCodeInstances = new List<CheatCodeInstance>();
+    // Update is called once per frame
+    void Update()
+    {
+        Cheater();
+    }
+    void Cheater()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (playerTyping)
+            {
+                CheckCheat(currentString);
+            }
+            playerTyping = !playerTyping;
+        }
+        if (playerTyping)
+        {
+            foreach (char c in Input.inputString)
+            {
+                if (c == '\b')
+                {
+                    if (currentString.Length > 0)
+                    {
+                        currentString = currentString.Substring(0, currentString.Length - 1);
+                    }
+                }
+                else if (c == '\n' || c == '\r')
+                {
+                    currentString = "";
+                }
+                else
+                {
+                    currentString += c;
+                }
+            }
+        }
+    }
+
+    private void CheckCheat(string currentString)
+    {
+        currentString = currentString.ToLower();
+        foreach (CheatCodeInstance code in cheatCodeInstances)
+        {
+            if (currentString == code.code)
+            {
+                code.cheatEvent?.Invoke();
+                break;
+            }
+        }
+    }
+
+    public void DawiCode()
+    {
+        print("I AM RACIST AND HORNY FOR SHOUTAS");
+    }
+
+}

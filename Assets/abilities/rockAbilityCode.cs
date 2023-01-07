02b411fa868b3b2a3cb08e0ddd9fdba6940f97af
Firas-Hilabi <firasofficial2000@gmail.com>
2023-01-07 21:17:47 +0300

    adjustments on explosion

--- Assets/abilities/rockAbilityCode.cs
+++ Assets/abilities/rockAbilityCode.cs
@@ -6,7 +6,20 @@ using UnityEngine;
 public class rockAbilityCode : MonoBehaviour
 {
     public ParticleSystem explosion;
+    public LayerMask layer;
 
+    int timer = 0;
+
+    void Update(){
+        if(timer >= 142){
+            Instantiate(explosion, transform.position, Quaternion.identity);
+        }
+    }
+
+    void FixedUpdate(){
+        timer++;
+        if(timer >= 143) timer = 0;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "enemy")
@@ -16,8 +29,13 @@ public class rockAbilityCode : MonoBehaviour
             VictoryMenu victoryMenu = temp.GetComponent<VictoryMenu>();
             victoryMenu.numberOfEnemiesDestroyed++;
             enemy.giveGoldOnDeath();
-            Instantiate(explosion, transform.position, Quaternion.identity);
+            //Instantiate(explosion, transform.position, Quaternion.identity);
             Destroy(enemy.gameObject);
+            Debug.Log(timer);
+        }
+
+        if(other.gameObject.layer == layer.value){
+            Instantiate(explosion, transform.position, Quaternion.identity);
         }
         
     }
